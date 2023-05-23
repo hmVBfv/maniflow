ff5285eb51a7238f270a493729035acd7fff3fbe
fwidmaier <felixwidmaier@gmx.de>
2023-05-23 15:31:45 +0200

    Added documentation to the VertexFunction class

--- maniflow/mesh/utils.py
+++ maniflow/mesh/utils.py
@@ -3,14 +3,34 @@ from mesh import Mesh
 
 
 class VertexFunction(object):
+    """
+    A wrapper class to decorate certain methods that modify the
+    vertices in a given mesh.
+    """
     def __init__(self, func: callable):
-        self.func = func
+        """
+        Initializes the wrapper class and stores the function that is
+        to be wrapped.
+        :param func:
+        """
+        self.func = func  # the  wrapped  function
         functools.update_wrapper(self, func)
 
-    def __call__(self, mesh: Mesh, *args, **kwargs):
-        new = mesh.copy()
-        for i in range(len(new.vertices)):
+    def __call__(self, mesh: Mesh, *args, **kwargs) -> Mesh:
+        """
+        This method lets the provided function act on all vertices in the given
+        mesh. A (deep) copy of the original mesh is then returned
+        :param mesh: the mesh the function should act on
+        :param args: optional arguments
+        :param kwargs:
+        :return: the resulting mesh where every vertex in the mesh was modified  by the
+        given function
+        """
+        new = mesh.copy()  # we create a (deep) copy of the original mesh
+
+        for i in range(len(new.vertices)):  # now we apply the function to all vertices on the mesh
             new.vertices[i] = self.func(new.vertices[i])
 
-        new.updateNormals()
+        new.updateNormals()   # as the vertices have changed we need to update the surface normals
+
         return new
