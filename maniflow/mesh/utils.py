e2fcc354265772131d4aba0963cbe29465da685d
fwidmaier <felixwidmaier@gmx.de>
2023-05-23 23:38:06 +0200

    Added methods for determining the orientability of a mesh

--- maniflow/mesh/utils.py
+++ maniflow/mesh/utils.py
@@ -1,5 +1,43 @@
 import functools
-from mesh import Mesh
+import numpy as np
+from mesh import Mesh, Face
+
+
+def adjacentFaces(mesh: Mesh, vertex: int) -> list[Face]:
+    """
+    A method to determine the adjacent faces of a given vertex
+    :param mesh: the mesh on which the vertex is
+    :param vertex: the vertex from which the adjacent areas are to be determined
+    :return: a list containing the adjacent faces of the given vertex
+    """
+    adjacent = list()  # we will store the adjacent faces in this list
+    for face in mesh.faces:  # traverse all faces of the mesh
+        if vertex in face.vertices:  # determine whether the face is adjacent to the vertex
+            adjacent.append(face)
+    return adjacent
+
+
+def isOrientable(mesh: Mesh) -> bool:
+    """
+    A method to determine whether a given mesh is orientable or not.
+    In general, a manifold is orientable if there is a non-vanishing continuous normal field.
+    To check whether the mesh is orientable or not, we traverse all vertices in the mesh and check whether
+    the adjacent surfaces are oriented in the same way. We can do this by looking at the normal vectors of
+    the faces and forming all possible scalar products. These all have to be non-negative.
+    Otherwise, we are dealing with a non-orientable surface.
+    :param mesh: the mesh for which the decision should be made whether it is orientable
+    :return: True of it is orientable. Otherwise, False.
+    """
+    for vertex in range(len(mesh.vertices)):  # traverse all vertices in the mesh
+        adjacentNormals = list(map(lambda face: face.normal,  # we determine the normal vectors of the adjoining faces
+                                   adjacentFaces(mesh, vertex)))  # and store them in a list
+        for normal in adjacentNormals[1::]:  # we only need to compare the orientation of each vector to the first
+            # vector in the list
+            if np.dot(normal, adjacentNormals[0]) < 0:
+                # in this case, the vectors do not have the same orientation and
+                # the mesh is not orientable
+                return False
+    return True
 
 
 def eulerCharacteristic(mesh: Mesh) -> int:
