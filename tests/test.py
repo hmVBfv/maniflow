f9821ad0a3314485b40f77e40f447e8451077c24
fwidmaier <felixwidmaier@gmx.de>
2023-06-03 23:23:12 +0200

    Updated the file paths to the example files

--- tests/test.py
+++ tests/test.py
@@ -6,16 +6,17 @@ from maniflow.mesh.obj import OBJFile
 
 class TestConnectionComponents(unittest.TestCase):
     def test__connection_components(self):
-        self.assertEqual(len(connectedComponents(OBJFile.read("cube.obj"))), 1)
-        self.assertEqual(len(connectedComponents(OBJFile.read("cone.obj"))), 1)
-        self.assertEqual(len(connectedComponents(OBJFile.read("untitled.obj"))), 2)
+        self.assertEqual(len(connectedComponents(OBJFile.read("examples/cube.obj"))), 1)
+        self.assertEqual(len(connectedComponents(OBJFile.read("examples/cone.obj"))), 1)
+        self.assertEqual(len(connectedComponents(OBJFile.read("examples/test.obj"))), 2)
 
 
 class TestOrientability(unittest.TestCase):
     def test_orientability(self):
-        self.assertFalse(isOrientable(OBJFile.read("test.obj")), "moebius strip should not be orientable")
-        self.assertTrue(isOrientable(OBJFile.read("teapot.obj")), "teapot should be orientable")
-        self.assertTrue(isOrientable(OBJFile.read("torus.obj")), "torus should be orientable")
+        self.assertTrue(isOrientable(OBJFile.read("examples/cube.obj")))
+        self.assertFalse(isOrientable(OBJFile.read("examples/moebius.obj")), "moebius strip should not be orientable")
+        self.assertTrue(isOrientable(OBJFile.read("examples/teapot.obj")), "teapot should be orientable")
+        self.assertTrue(isOrientable(OBJFile.read("examples/torus.obj")), "torus should be orientable")
 
 
 if __name__ == "__main__":
