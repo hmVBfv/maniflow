0c4ae3c7150fd6887240680c19fc80c7f950bb9f
Mark Robin Riegraf <mark.riegraf@stud.uni-goettingen.de>
2023-06-16 13:36:42 +0200

    Outline of code to identify and mapping coinciding vertices

--- maniflow/mesh/utils.py
+++ maniflow/mesh/utils.py
@@ -1,4 +1,5 @@
 import functools
+import numpy as np
 from maniflow.mesh import Mesh, Face
 
 
@@ -53,6 +54,37 @@ def getBoundaryVertices(mesh: Mesh) -> list[int]:
     :return: a list of all boundary vertices from the mesh
     """
     return [v for v in range(mesh.v) if isBoundaryVertex(v, mesh)]
+
+
+def coincidingVertices(mesh: Mesh):
+    """
+    A method to identify vertices with the same coordinates with each other.
+    This way we can "clue" edges together that share the same coordinates of technically different vertices.
+    The approach is similar to a upper triangle matrix as we don't need to reverse check vertices,
+    e.g. v1==v2 doesn't require additional v2==v1 check.
+    :param mesh: the mesh of which vertices should be 
+    """
+    verts = list()
+    lookup = dict()
+
+    for i in range(mesh.v-1):
+        # i in lookup implies that the i-th vertex is equal some previous i'-th vertex with i'<i
+        # thus was taken care of as i+1+j in the following steps
+        if i in lookup:
+            continue
+        for j in range(mesh.v-i-1):
+            # taken care of in previous iteration of some smaller i
+            if i+1+j in lookup:
+                continue
+            if np.allclose(mesh.vertices[i], mesh.vertices[i+1+j], atol=1e-06):  # vertex coordiantes are same or at least very close
+                lookup[i+1+j] = len(verts)  # linking to index in new vertex list
+        lookup[i] = len(verts)  # adding i to dict as i not in lookup before (see "if" above)
+        verts.append(mesh.vertices[i])
+
+    # update the faces
+    mesh.vertices = verts
+    for face in mesh.faces:
+        face.vertices = [lookup[i] for i in face.vertices]  # update the face links
 
 
 def _normal_form(face1: tuple[int], face2: tuple[int]) -> list[tuple[int]]:
