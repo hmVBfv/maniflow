f2a92d9f576e1737e98850d62c538e4317ce1759
fwidmaier <felixwidmaier@gmx.de>
2023-09-05 17:36:48 +0200

    Cleaned up some code, fixed Brensenham's line algorithm, added a test shader (very sketchy at the moment) - overall the code is still a mess. Further cleaning and documenting is needed

--- maniflow/render/scene.py
+++ maniflow/render/scene.py
@@ -3,6 +3,29 @@ import pyrr  # TODO: get rid of pyrr. It is only used to compute the projection 
 import numpy as np
 from PIL import Image
 
+from tqdm import tqdm
+
+
+def test_shader(face):
+    shininess = 200
+    L = pyrr.vector.normalize(np.float32([15,10,15]))  # 10,-10,50
+    E = np.float32([0,0,1])
+    H = pyrr.vector.normalize(L + E)
+    p0, p1, p2 = face[0], face[1], face[2]
+    N = pyrr.vector.normalize(pyrr.vector3.cross(p1 - p0, p2 - p0))
+    ff = pyrr.vector.normalize(np.array([0,0,0]) + L)
+    if np.dot(ff, N) < 0:
+
+        return dict(fill=[252, 185, 15], opacity=100, stroke=[100,100,100,50])
+        #return None
+    # print(N)
+    df = max(0, np.dot(N, L))
+    sf = pow(max(0, np.dot(N, H)), shininess)
+    color = df * np.float32([1, 0.72, 0.05]) + sf * np.float32([1, 1, 1])
+    color = np.power(color, 1.0 / 2.2)
+    color *= 255
+    return dict(fill=color, opacity=200, stroke=[100,100,100,200])
+
 
 def baryCentricCoordinates(a: np.array, b: np.array, c: np.array, x: int, y: int) -> np.array:
     """
@@ -51,16 +74,39 @@ def getBoundingBox(a: np.array, b: np.array, c: np.array) -> list[list[float]]:
 
 def rasterizeLine(x0, y0, x1, y1, color, img):
     # Bresenham's line algorithm (wiki)
-    dx = x1 - x0
-    dy = y1 - y0
-    D = 2 * dy - dx
-    y = y0
-    for x in range(x0, x1):
-        img[x][y] = color
-        if D > 0:
-            y += 1
-            D -= 2 * dx
-        D += 2 * dy
+    dx = abs(x1 - x0)
+    dy = -abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
+    error = dx + dy
+    e2 = 0
+
+    while True:
+        try:
+            # img[x0][y0] = [0,0,0,255]#color
+            img[x0][y0][:3:] = 255 * np.ones(3) - np.sqrt(((255 * np.ones(3) - img[x0][y0][:3:] * img[x0][y0][3] / 255) ** 2 +
+                                                         (255 * np.ones(3) - np.array(color[:3:]) * color[
+                                                             3] / 255) ** 2) / 2)
+            img[x0][y0][3] = img[x0][y0][3] + color[3] if img[x0][y0][3] + color[3] <= 255 else 255
+            #img[x0][y0][:3:] = 255 * np.ones(3) - (500 * np.ones(3) - (img[x0][y0][:3:]) - np.array(color[:3:])) / 2
+            #img[x0][y0][3] = img[x0][y0][3] + color[3] if img[x0][y0][3] + color[3] <= 255 else 255
+        except Exception as e:
+            # print("hello! i am under the water. please help me")
+            pass
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2 * error
+        if e2 >= dy:
+            if x0 == x1:
+                break
+            error += dy
+            x0 += sx
+        if e2 <= dx:
+            if y0 == y1:
+                break
+            error += dx
+            y0 += sy
+    return img
 
 
 def rasterizeTriangle(a: np.array, b: np.array, c: np.array, color: np.array, img: np.array) -> np.array:
@@ -80,8 +126,8 @@ def rasterizeTriangle(a: np.array, b: np.array, c: np.array, color: np.array, im
     :return: the image buffer with the rasterized triangle
     """
     box = getBoundingBox(a, b, c)
-    for x in range(int(box[0][0]), int(box[1][0])):  # we scan every point (x,y) in the bounding box
-        for y in range(int(box[0][1]), int(box[1][1])):
+    for x in range(int(np.floor(box[0][0])), int(np.ceil(box[1][0]))):  # we scan every point (x,y) in the bounding box
+        for y in range(int(np.floor(box[0][1])), int(np.ceil(box[1][1]))):
             baryCentric = baryCentricCoordinates(a, b, c, x, y)  # we compute the barycentric coordinates
             inTriangle = np.all(baryCentric >= 0)
             # now we check whether this point is inside the image buffer (if not we discard it)
@@ -89,14 +135,41 @@ def rasterizeTriangle(a: np.array, b: np.array, c: np.array, color: np.array, im
                 continue
 
             # if the point lies within the image buffer and in the triangle we apply the color to it
-            img[x][y] = color
+            if np.all(img[x][y] == 0):
+                img[x][y] = np.array(color)
+            else:
+                img[x][y][:3:] = 255 * np.ones(3) - np.sqrt(((255 * np.ones(3) - img[x][y][:3:]*img[x][y][3]/255)**2 +
+                                                            (255 * np.ones(3) - np.array(color[:3:])*color[3]/255)**2) / 2)
+                #print(img[x][y][:3:])
+                #img[x][y][:3:] = 255 * np.ones(3) - (500 * np.ones(3) - (img[x][y][:3:]) - np.array(color[:3:]))/2
+                img[x][y][3] = img[x][y][3] + color[3] if img[x][y][3] + color[3] <= 255 else 255
+                #img[x][y] = np.array(color)
 
     return img
 
 
+def rasterizePolygon(face, image, fill=[255,255,255], opacity=255, stroke=[0,0,0,255]):
+
+    fill = list(fill)
+    fill.append(opacity)
+    if len(face) == 3:
+        image = rasterizeTriangle(face[0], face[1], face[2], color=fill, img=image)
+    if len(face) == 4:
+        image = rasterizeTriangle(face[0], face[1], face[2], color=fill, img=image)
+        image = rasterizeTriangle(face[0], face[3], face[2], color=fill, img=image)
+
+    pp = face[0]
+    for p in face[1::]:
+        image = rasterizeLine(int(pp[0]), int(pp[1]), int(p[0]), int(p[1]), color=stroke, img=image)
+        pp = p
+    image = rasterizeLine(int(face[0][0]), int(face[0][1]), int(face[-1][0]), int(face[-1][1]), color=stroke, img=image)
+
+    return image
+
+
 class Camera:
     def __init__(self, position: np.array, target: np.array = (0, 0, 0), up: np.array = (0, 1, 0),
-                 fovy: float = 15, aspect: float = 1, near: float = 10, far: float = 200):
+                 fovy: float = 15, aspect: float = 1.5, near: float = 10, far: float = 200):
         """
         Initializes a new pinhole camera with the given parameters.
         :param position: A numpy array (3d) where the camera is located
@@ -121,27 +194,43 @@ class Render:
         self.camera = camera
 
     def render(self, mesh: Mesh):
-        image = np.zeros((500, 500, 4))
+        image = np.zeros((1080, 720, 4))
         # creating one array from the mesh. Encoding all of its geometry into it
         verts = np.float32(list(map(tuple, mesh.vertices)))
         getids = lambda face: tuple(face.vertices)
         faces = np.int32(list(map(getids, mesh.faces)))
         faces = verts[faces]
-        # print(faces)
+        faces *= 0.45
+        Faces = faces.copy()
         faces = np.dstack([faces, np.ones(faces.shape[:2])])
         faces = np.dot(faces, self.camera.projection)
-        # print(faces)
         xyz, w = faces[:, :, :3], faces[:, :, 3:]
         # Apply perspective transformation.
         xyz, w = faces[:, :, :3], faces[:, :, 3:]
         faces = xyz / w
 
+        # sort faces from back to front.
+        centroids = -np.sum(w, axis=1)
+        for i in range(len(centroids)):
+            centroids[i] /= len(faces[i])
+        centroids = centroids.ravel()
+        # print(centroids)
+        face_indices = np.argsort(centroids)
+        faces = faces[face_indices]
+        Faces = Faces[face_indices]
+        # print(faces)
+        # faces = faces[face_indices]
+
         faces[:, :, 0:1] = (1.0 + faces[:, :, 0:1]) * image.shape[0] / 2
         faces[:, :, 1:2] = (1.0 - faces[:, :, 1:2]) * image.shape[1] / 2
-
-        for face in faces:
+        print(len(faces))
+        for i, face in tqdm(enumerate(faces)):
             face = np.around(face[:, :2], 5)
-            image = rasterizeTriangle(face[0], face[1], face[2], color=[255, 255, 255, 255], img=image)
+            # image = rasterizeTriangle(face[0], face[1], face[2], color=[255, 255, 255, 100], img=image)
+            style = test_shader(Faces[i])
+            if style is None:
+                continue
+            image = rasterizePolygon(face, image, **style)
 
         iimage = Image.fromarray(image.transpose((1, 0, 2)).astype(np.uint8), "RGBA")
-        iimage.save("testing.png")
+        iimage.save("testing2.png")
