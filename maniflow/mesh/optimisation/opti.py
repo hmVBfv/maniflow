51b1e9ebd220d18c999452092f149d963bd9982d
Mark Robin Riegraf <mark.riegraf@stud.uni-goettingen.de>
2023-09-14 14:00:06 +0200

    Add initial function outline for mesh optimization

---
+++ maniflow/mesh/optimisation/opti.py
@@ -0,0 +1,21 @@
+import functools
+import numpy as np
+from maniflow.mesh import Mesh, Face
+
+def optimiseVertexPos():
+    pass
+
+def projectPoints():
+    pass
+
+def improveVertexPos():
+    pass
+
+def optimiseMesh():
+    pass
+
+def generateLegalMove():
+    pass
+
+def conjugateGradiant():
+    pass
