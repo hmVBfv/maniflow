3d2830e2824fcba4cb407b3eb2760977a4012118
Mark Robin Riegraf <mark.riegraf@stud.uni-goettingen.de>
2023-09-14 14:57:37 +0200

    Added explanatory comments for mesh optimization functions and operations

--- maniflow/mesh/optimisation/opti.py
+++ maniflow/mesh/optimisation/opti.py
@@ -2,20 +2,68 @@ import functools
 import numpy as np
 from maniflow.mesh import Mesh, Face
 
+"""
+The mesh optimisation is based on the minimisation of an energy function which rewards
+a mesh being as close as possible to the initial points but penalises an increasing number of vertices.
+It consists of the sum of
+* E_dist, the sum of squared distances between the mesh and the points
+* E_rep, a penaliser proportional to the amount of vertices used
+* E_spring, a spring energy of rest length zero to ensure the existence of a minimum to the overall energy equation.
+"""
+
+"""
+Since E_rep is depending on the amount of vertices and not their acutal positions,
+optimiseVertexPos() is trying to minimise the sum E_dist + E_spring.
+It uses projectPoints() and improveVertexPos().
+"""
 def optimiseVertexPos():
     pass
 
+"""
+Calculate the optimal barycentric coordinate vectors B for given fixed vertex positions V using projection
+A naive approach as to project each point onto each of the faces might be too inefficient.
+To avoid that one could try to find only subsets of nearby faces against the respective points.
+"""
 def projectPoints():
     pass
 
+"""
+Fixing barycentric coodinate vectors B and the general structure of the mesh in tact while minimising over the vertex position V.
+This represents a linear least square challenge, which can be broken down into three separate and self-contained subproblems,
+each corresponding to one of the three vertex position coordinates.
+We apply the conjugate gradient method to solve the problems.
+"""
 def improveVertexPos():
     pass
 
+"""
+Implement the Conjugate Gradient method for iterative optimization.
+This method efficiently solves linear systems of equations by iteratively
+minimizing the residual error updating a conjugate direction vector.
+"""
+def conjugateGradiant():
+    pass
+
+"""
+To optimise the mesh structure we us three elementary transformations:
+* edge collapse, eliminating an edge and identifying its endpoints with eachother
+* edge split, introducing another vertex on the edge which in turn spawn new edges to vertices that are adjacent to both former endpoints.
+* edge swap, the edge no connects vertices that are adjacent to both former endpoints instead of said former endpoints.
+"""
 def optimiseMesh():
     pass
 
+"""
+The operation of splitting an edge is inherently a valid move, as it never alters the topological structure of the mesh.
+Conversely, the remaining two transformations possess the potential to modify the topological structure,
+hence the need to check whether they can be applied.
+We check whether an operation reduces the energy and is legal and decide accordingly.
+* Edge splitting is always legal.
+* The edge collapse between points i and j is legal if and only if all vertices k adjacent to i and j form faces {i,j,k} as well as
+    ^ If i and j are boundary vertices, then the edge {i,j} is a boundary edge
+    ^ If neither i nor j are boundary vertices, then the local mesh as at least four vertices
+    ^ Otherwise, the local mesh has at least three vertices.
+* An edge swap is only legal if and only if the there exists new edge between the potential two new endpoints of the edge swap.
+"""
 def generateLegalMove():
-    pass
-
-def conjugateGradiant():
     pass
