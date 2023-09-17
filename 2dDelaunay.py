24eb4b2af53b02c1450e2526b79b9e59831d2f39
Yangshan Xiang <yangshan.xiang@stud.uni-goettingen.de>
2023-09-17 13:40:36 +0200

    Implemented a new method to compute the cirum-circle for a given triangle

--- 2dDelaunay.py
+++ 2dDelaunay.py
@@ -3,6 +3,11 @@ import numpy as np
 
 
 def superTriangle(vertices: list[list]) -> list[list]:
+    """
+    A method to determine the super triangle, which is the first step in Bowyer–Watson algorithm.
+    :param vertices: the given list of vertices
+    :return: the super triangle which contains all the given vertices
+    """
     vertices = np.array(vertices)
     x = vertices[:, 0]
     y = vertices[:, 1]
@@ -19,5 +24,65 @@ def superTriangle(vertices: list[list]) -> list[list]:
 
     return [v, v1, v2]
 
-# def BowyerWatson2d(vertices: list):
+
+def circumCircle(triangle: list[list]) -> dict:
+    """
+    A method to determine the circum-circle of a given triangle.
+    :param triangle: the target triangle
+    :return: the circum-circle which is defined by its centre & radius
+    """
+
+    if triangle[0][0] - triangle[1][0] == 0:
+        # If slope doesn't exist, then the slope after rotating 90° is 0
+        k1 = 0
+        if triangle[1][1] - triangle[2][1] == 0:
+            # if slope is 0, then the slope after rotating 90° is infinity
+            k2 = 'infinity'
+        else:
+            k2 = -1 / ((triangle[1][1] - triangle[2][1]) / (triangle[1][0] - triangle[2][0]))
+    elif triangle[1][0] - triangle[2][0] == 0:
+        # If slope doesn't exist, then the slope after rotating 90° is 0
+        k2 = 0
+        if triangle[0][1] - triangle[1][1] == 0:
+            # if slope is 0, then the slope after rotating 90° is infinity
+            k1 = 'infinity'
+        else:
+            k1 = -1 / ((triangle[0][1] - triangle[1][1]) / (triangle[0][0] - triangle[1][0]))
+    else:
+        if triangle[0][1] - triangle[1][1] == 0:
+            # if slope is 0, then the slope after rotating 90° is infinity
+            k1 = 'infinity'
+        else:
+            k1 = -1 / ((triangle[0][1] - triangle[1][1]) / (triangle[0][0] - triangle[1][0]))
+        if triangle[1][1] - triangle[2][1] == 0:
+            # if slope is 0, then the slope after rotating 90° is infinity
+            k2 = 'infinity'
+        else:
+            k2 = -1 / ((triangle[1][1] - triangle[2][1]) / (triangle[1][0] - triangle[2][0]))
+
+    mid1 = [(triangle[0][0] + triangle[1][0]) / 2, (triangle[0][1] + triangle[1][1]) / 2]
+    mid2 = [(triangle[1][0] + triangle[2][0]) / 2, (triangle[1][1] + triangle[2][1]) / 2]
+
+    if triangle[0][1] - triangle[1][1] == 0:
+        # If slope equals to 0, then the slope after rotating 90° doesn't exist
+        x = mid1[0]
+        y = k2 * (x - mid2[0]) + mid2[1]
+        print(k2)
+    elif triangle[1][1] - triangle[2][1] == 0:
+        # If slope equals to 0, then the slope after rotating 90° doesn't exist
+        x = mid2[0]
+        y = k1 * (x - mid1[0]) + mid1[1]
+    else:
+        x = (k1 * mid1[0] - k2 * mid2[0] + mid2[1] - mid1[1]) / (k1 - k2)
+        y = k1 * (x - mid1[0]) + mid1[1]
+
+    r = ((x - triangle[0][0]) ** 2 + (y - triangle[0][1]) ** 2) ** 0.5
+    # Compute radius
+    centre = [x, y]
+
+    return {'centre': centre, 'radius': r}
+
+
+# def BowyerWatson2d(vertices: list[list]):
+#
 #     return
