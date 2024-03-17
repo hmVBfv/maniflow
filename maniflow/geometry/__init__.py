ee45fc22e5b092ab4f0bfc47dd250878942e3460
Felix Widmaier <felixwidmaier@gmx.de>
2024-03-17 13:56:24 +0100

    I added docstrings and documentation to the methods gaussian and vertexAngle.

--- maniflow/geometry/__init__.py
+++ maniflow/geometry/__init__.py
@@ -7,4 +7,14 @@ import numpy as np
 class Curvature:
     @staticmethod
     def gaussian(mesh: Mesh, vertex: int) -> float:
+        """
+        A method to compute the (discrete) Gaussian curvature of a given mesh
+        at a specified vertex.
+        The discrete Gaussian curvature is given as the angle defect of
+        the sum of the angles of the vertex in the adjacent faces.
+        :param mesh: the mesh on where to perform the computations on
+        :param vertex: the vertex at which the curvature is to be evaluated
+        :return: the (discrete) Gaussian curvature of the mesh at the vertex
+        """
+
         return 2 * np.pi - sum(vertexAngle(mesh, vertex, face) for face in adjacentFaces(mesh, vertex))
