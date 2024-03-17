6ba0e1e23a59424996f562f426b267e1987e5da2
Felix Widmaier <felixwidmaier@gmx.de>
2024-03-17 13:43:37 +0100

    Implemented discrete gaussian curvature

--- maniflow/geometry/__init__.py
+++ maniflow/geometry/__init__.py
@@ -1,7 +1,10 @@
 from maniflow.mesh import Mesh
+from maniflow.mesh.utils import adjacentFaces
+from maniflow.geometry.utils import vertexAngle
+import numpy as np
 
 
 class Curvature:
     @staticmethod
     def gaussian(mesh: Mesh, vertex: int) -> float:
-        pass
+        return 2 * np.pi - sum(vertexAngle(mesh, vertex, face) for face in adjacentFaces(mesh, vertex))
