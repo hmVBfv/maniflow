23dd6cbac2eb18eecf09c132d537141faa1a21a1
Mark Robin Riegraf <mark.riegraf@stud.uni-goettingen.de>
2024-04-18 21:01:32 +0200

    Add comments for clearification
    Still not working...

--- maniflow/mesh/simplification/simplify.py
+++ maniflow/mesh/simplification/simplify.py
@@ -87,40 +87,71 @@ def optimalContractionPoint(mesh: Mesh, vert1: int, vert2: int, Q1: np.array, Q2
     if np.linalg.det(Q) != 0:
         vbar = np.dot(np.linalg.inv(Q), np.array([0, 0, 0, 1]))
     else:   # If not invertible take the best point between the initial points and their midpoint
-        v1 = np.hstack([mesh.vertices[vert1], [1]])
-        v2 = np.hstack([mesh.vertices[vert2], [1]])
-        v12 = np.array([(v1+v2)[0] / 2, (v1+v2)[1] / 2, (v1+v2)[2] / 2, 1])
-        vbar = min(np.dot(v1, np.dot(Q, v1)), np.dot(v2, np.dot(Q, v2)), np.dot(v12, np.dot(Q, v12)))
+        v1 = np.hstack([mesh.vertices[vert1], [1]]) # adding 1 to have a 4 entry vector to treat as vbar
+        v2 = np.hstack([mesh.vertices[vert2], [1]]) # adding 1 to have a 4 entry vector to treat as vbar
+        v12 = np.array([(v1+v2)[0] / 2, (v1+v2)[1] / 2, (v1+v2)[2] / 2, 1]) # midpoint between v1 and v2 and adding 1 to have a 4 entry vector to treat as vbar
+        vbar = min(np.dot(v1, np.dot(Q, v1)), np.dot(v2, np.dot(Q, v2)), np.dot(v12, np.dot(Q, v12))) # comparing and getting the minimal cost
     return Q, vbar
 
 def rewriteFaces(mesh: Mesh, a: int, b: int, Q1: np.array, Q2: np.array):
-    _, vbar = optimalContractionPoint(mesh, a, b,  Q1, Q2)
+    """
+    A method to rewrite faces of contracting vertices.
+    Either the face becomes degenerative in which case we denote all its vertices to be the same for later deletion
+    or we update the coordinates of the contracting vertices to the contracting point vbar.
+
+    :param mesh: Mesh we are working with
+    :param a: First vertex involved in the contraction
+    :param b: Second vertex involved in the contraction
+    :param Q1: Characterizing matrix of the first vertex
+    :param Q2: Characterizing matrix of the second vertex
+    :return: Mesh with updated face and vertex values
+    """
+    _, vbar = optimalContractionPoint(mesh, a, b,  Q1, Q2) # get the optimal contraction point between a and b
     adjFaces = adjacentFaces(mesh, a, indices=True)
-    adjFaces = adjFaces + adjacentFaces(mesh, b, indices=True)
+    adjFaces = adjFaces + adjacentFaces(mesh, b, indices=True) # combining the adjacent faces of a and b
+    # for every face adjacent to either a and / or b we check whether it contains the two
     for face in adjFaces:
         if (a in mesh.faces[face].vertices) and (b in mesh.faces[face].vertices):
-            mesh.faces[face].vertices = (b, b, b)
+            mesh.faces[face].vertices = (b, b, b) # degenerate face
         else:
             for i in range(3):
+                # update the index of the face vertex which was refering to b
                 if mesh.faces[face].vertices[i] == b:
+                    # using workaround as tuple are not writable while lists are
                     tmp_list = list(mesh.faces[face].vertices)
                     tmp_list[i] = a
                     mesh.faces[face].vertices = tuple(tmp_list)
-    mesh.vertices[a] = vbar[:3]
+    mesh.vertices[a] = vbar[:3] # a gets the coordinates of vbar minus the 1 element (vbar=[x,y,z,1])
     return mesh
 
-def normalsFlipped(mesh: Mesh, a: int, b: int, Q1: np.array, Q2: np.array):
-    mesh2 = mesh.copy()
+def normalsFlipped(mesh: Mesh, a: int, b: int, Q1: np.array, Q2: np.array) -> bool:
+    """
+    Method to check whether the normals will be flipped after a contraction of a and b.
+    This will be used to disqualify the vertex pairs that cause a mesh inversion if contracted.
+
+    :param mesh: Mesh we are working with
+    :param a: First vertex involved in the contraction
+    :param b: Second vertex involved in the contraction
+    :param Q1: Characterizing matrix of the first vertex
+    :param Q2: Characterizing matrix of the second vertex
+    :return: Boolean whether the normals have been flipped or not
+    """
+
+    mesh2 = mesh.copy() # an independent copy of the initial mesh to work on
     adjFaces = adjacentFaces(mesh2, a, indices=True)
-    adjFaces = adjFaces + adjacentFaces(mesh2, b, indices=True)
+    adjFaces = adjFaces + adjacentFaces(mesh2, b, indices=True) # adjacent faces of a and b combined
     norm_dict = {}
+    # for each face as key as its normal as value to the dictionary
     for face in adjFaces:
         norm_dict[face] = mesh2.faces[face].normal
-    mesh2 = rewriteFaces(mesh2, a, b, Q1, Q2)
-    adjFaces_new = adjacentFaces(mesh2, a, indices=True)
+    mesh2 = rewriteFaces(mesh2, a, b, Q1, Q2) # rewrite the faces so that the contraction happened on mesh2
+    adjFaces_new = adjacentFaces(mesh2, a, indices=True) # get the adjacent faces of the contraction point
+    # for each face adjacent to a check whether normals flipped
     for face_new in adjFaces_new:
+        # if flipped
         if np.dot(mesh2.faces[face_new].normal, norm_dict[face_new]) < 0:
             return True
+        # if not flipped
         else:
             return False
 
@@ -148,7 +179,7 @@ def contractingCost(mesh: Mesh, vert1: int, vert2: int, Q1: np.array, Q2: np.arr
 def getValidPairs(mesh: Mesh, tol = 0) -> np.array:
     """
     Returns an adjacency matrix indicating valid pairs of vertices in the mesh.
-    Validity is either given by two vertices sharing an edge or by them being sufficiently close together
+    Validity is either given by two vertices sharing an edge or by them being sufficiently close together.
 
     :param mesh: The mesh from which we want to get the valid vertex pairs
     :param tol: The tolerance for "closeness" of disconnected vertices
@@ -162,10 +193,12 @@ def getValidPairs(mesh: Mesh, tol = 0) -> np.array:
     for i in range(mesh.f):
         for j in range(3):
             for k in range(3):
+                # Ignore boundary vertices
                 if (mesh.faces[i].vertices[j] not in boundaryVerts) and (mesh.faces[i].vertices[j] not in boundaryVerts):
                     validityMatrix[mesh.faces[i].vertices[j], mesh.faces[i].vertices[k]] = 1
                     validityMatrix[mesh.faces[i].vertices[k], mesh.faces[i].vertices[j]] = 1
     
+    # This is a draft for a non-zero value of 'tol', untested as simplifyByContraction still throws errors
     """
     # tol != 0 implies that we want to do non-edge contraction as well
     if tol != 0:
@@ -174,7 +207,7 @@ def getValidPairs(mesh: Mesh, tol = 0) -> np.array:
         for i in range(mesh.v):
             for j in range(i, mesh.v):
                                if (a == j) or (b == j):
- # Ignore adjacent vertices for efficiency
+                # Ignore adjacent vertices for efficiency
                 if validityMatrix[face.vertices[i], face.vertices[j]] != 1:
                     # If within tolerance, mark vertices down with 2 to be able to differentiate
                     if np.linalg.norm(face.vertices[i], face.vertices[j]) < tol:
@@ -183,15 +216,17 @@ def getValidPairs(mesh: Mesh, tol = 0) -> np.array:
 
     return validityMatrix
 
-def simplifyByContraction(mesh: Mesh, tol = 0, reduction = 0.95):
-    # TODO:
-    # Proper documentation and comments
-    # TODO:
-    # Check for potential mesh inversion (preserve orientation)
-    # Compare the normal of each neighboring face before and after the contraction
-    # -> If change then disallow or penalize
-    # TODO:
-    # Rigorous testing
+def simplifyByContraction(mesh: Mesh, tol = 0, reduction = 0.95) -> Mesh:
+    """
+    A method to simplify the mesh by contracting vertices.
+    It makes use of characteristic matrix Q for each vertex and deploys a cost function
+    to determine which vertex pair is best to contract next.
+
+    :param mesh: Mesh we are working with.
+    :param tol: Distance with which even unconnected vertices should be considered for contraction. Default 0 means no contraction.
+    :param reduction: Degree of reduction of the amount of faces. Either as percentage when <1, otherwise as absolute number
+    :return: Mesh with the desired reduction of faces
+    """
     
     tmp_mesh = mesh.copy()
     coincidingVertices(tmp_mesh) # 
@@ -212,14 +247,13 @@ def simplifyByContraction(mesh: Mesh, tol = 0, reduction = 0.95):
                 if not normalsFlipped(mesh, i, j, Q_list[i], Q_list[j]):
                     cost_dict[(i, j)] = contractingCost(tmp_mesh, i, j, Q_list[i], Q_list[j])
 
+    # sort the cost values and pop the first (lowest) value
     cost_dict = dict(sorted(cost_dict.items(), key=lambda item: item[1]))
     min_key = min(cost_dict, key=cost_dict.get)
-    a, b = min_key
-    del cost_dict[min_key]
+    a, b = min_key  # vertices a and b are to be contracted
+    del cost_dict[min_key]  # delete the used pair
 
     # Reduction parameter is either relative (<1) or absolute (integer)
-    # TODO:
-    # Sanity check
     if reduction < 1:
         reduction_goal = reduction * starting_amount_faces
     else:
@@ -238,19 +272,22 @@ def simplifyByContraction(mesh: Mesh, tol = 0, reduction = 0.95):
                     del cost_dict[(b, j)]
                 except:
                     del cost_dict[(j, b)]
+                # Update the validiy Matrix, meaning that we disconnect b and j and connect a and j
                 validityMatrix[j, b] = 0
                 validityMatrix[b, j] = 0
                 validityMatrix[j, a] = 1
                 validityMatrix[a, j] = 1
+                # Since a and j are connected with a having a new value, we need to update the cost between a and j
                 cost_dict[tuple(sorted((a, j)))] = contractingCost(tmp_mesh, tmp_mesh.vertices[a], tmp_mesh.vertices[j], Q_list[a], Q_list[j])
         
         # Cleaning up validity matrix as b now identified with a
         validityMatrix[b, :] = 0
         validityMatrix[:, b] = 0
             
-        tmp_mesh = rewriteFaces(tmp_mesh, a, b, Q_list[a], Q_list[b])
+        tmp_mesh = rewriteFaces(tmp_mesh, a, b, Q_list[a], Q_list[b]) # update faces with contraction point
 
-        adjFaces = sorted(adjacentFaces(tmp_mesh, a, indices=True))
+        adjFaces = sorted(adjacentFaces(tmp_mesh, a, indices=True)) # note that this is a list of incremental indices
+        # iterate through the adjacent faces
         for index in adjFaces:
             for i in range(3):
                 if tmp_mesh.faces[index].vertices[i] == a:
@@ -277,6 +314,7 @@ def simplifyByContraction(mesh: Mesh, tol = 0, reduction = 0.95):
         a, b = min_key
         del  cost_dict[min_key]
 
+    # Remove degenerate faces mark by having three times the same coordinate
     for face in tmp_mesh.faces:
         if face.vertices[0] == face.vertices[1] == face.vertices[2]:
             tmp_mesh.faces.remove(face)
